43aed50c636364bff337a44e758d3a2c9c29bbc7
Nick Bourdakos <bourdakos1@gmail.com>
2019-02-03 11:45:58 -0500

    fix bug

--- src/index.js
+++ src/index.js
@@ -1,152 +1,40 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import MagicDropzone from 'react-magic-dropzone'
 
 import * as tf from '@tensorflow/tfjs'
 import './styles.css'
 
 const MODEL_URL = './web_model/tensorflowjs_model.pb'
 const WEIGHTS_URL = './web_model/weights_manifest.json'
+const LABELS = ['Pepsi', 'Mountain Dew', 'Coke']
 
-class App extends React.Component {
-  state = {
-    model: null,
-    preview: '',
-    predictions: []
-  }
-
-  componentDidMount() {
-    tf.loadFrozenModel(MODEL_URL, WEIGHTS_URL).then(model => {
-      this.setState({
-        model: model
-      })
-    })
-  }
-
-  onDrop = (accepted, rejected, links) => {
-    this.setState({ preview: accepted[0].preview || links[0] })
-  }
-
-  cropToCanvas = (image, canvas, ctx) => {
-    const naturalWidth = image.naturalWidth
-    const naturalHeight = image.naturalHeight
-
-    canvas.width = image.width
-    canvas.height = image.height
-
-    ctx.clearRect(0, 0, ctx.canvas.width, ctx.canvas.height)
-    if (naturalWidth > naturalHeight) {
-      ctx.drawImage(
-        image,
-        (naturalWidth - naturalHeight) / 2,
-        0,
-        naturalHeight,
-        naturalHeight,
-        0,
-        0,
-        ctx.canvas.width,
-        ctx.canvas.height
-      )
-    } else {
-      ctx.drawImage(
-        image,
-        0,
-        (naturalHeight - naturalWidth) / 2,
-        naturalWidth,
-        naturalWidth,
-        0,
-        0,
-        ctx.canvas.width,
-        ctx.canvas.height
-      )
+const TFWrapper = model => {
+  const calculateMaxScores = (scores, numBoxes, numClasses) => {
+    const maxes = []
+    const classes = []
+    for (let i = 0; i < numBoxes; i++) {
+      let max = Number.MIN_VALUE
+      let index = -1
+      for (let j = 0; j < numClasses; j++) {
+        if (scores[i * numClasses + j] > max) {
+          max = scores[i * numClasses + j]
+          index = j
+        }
+      }
+      maxes[i] = max
+      classes[i] = index
     }
+    return [maxes, classes]
   }
 
-  onImageChange = e => {
-    const c = document.getElementById('canvas')
-    const ctx = c.getContext('2d')
-    this.cropToCanvas(e.target, c, ctx)
-
-    const batched = tf.tidy(() => {
-      const img = tf.fromPixels(c)
-      // Reshape to a single-element batch so we can pass it to executeAsync.
-      return img.expandDims(0)
-    })
-
-    const height = batched.shape[1]
-    const width = batched.shape[2]
-
-    this.state.model.executeAsync(batched).then(result => {
-      const boxes = result[0].dataSync()
-      const scores = result[1].dataSync()
-      const labels = result[2].dataSync()
-
-      // clean the webgl tensors
-      batched.dispose()
-      tf.dispose(result)
-
-      const prevBackend = tf.getBackend()
-      // run post process in cpu
-      tf.setBackend('cpu')
-      const indexTensor = tf.tidy(() => {
-        const boxes2 = tf.tensor2d(boxes, [
-          result[0].shape[1],
-          result[0].shape[2]
-        ])
-        return tf.image.nonMaxSuppression(
-          boxes2,
-          scores,
-          20, // maxNumBoxes
-          0.5, // iou_threshold
-          0.5 // score_threshold
-        )
-      })
-      const indexes = indexTensor.dataSync()
-      indexTensor.dispose()
-      // restore previous backend
-      tf.setBackend(prevBackend)
-
-      const predictions = this.buildDetectedObjects(
-        width,
-        height,
-        boxes,
-        scores,
-        indexes,
-        labels
-      )
-
-      // Font options.
-      const font = '16px sans-serif'
-      ctx.font = font
-      ctx.textBaseline = 'top'
-
-      predictions.forEach(prediction => {
-        const x = prediction.bbox[0]
-        const y = prediction.bbox[1]
-        const width = prediction.bbox[2]
-        const height = prediction.bbox[3]
-        // Draw the bounding box.
-        ctx.strokeStyle = '#00FFFF'
-        ctx.lineWidth = 4
-        ctx.strokeRect(x, y, width, height)
-        // Draw the label background.
-        ctx.fillStyle = '#00FFFF'
-        const textWidth = ctx.measureText(prediction.class).width
-        const textHeight = parseInt(font, 10) // base 10
-        ctx.fillRect(x, y, textWidth + 4, textHeight + 4)
-      })
-
-      predictions.forEach(prediction => {
-        const x = prediction.bbox[0]
-        const y = prediction.bbox[1]
-        // Draw the text last to ensure it's on top.
-        ctx.fillStyle = '#000000'
-        ctx.fillText(prediction.class, x, y)
-      })
-    })
-  }
-
-  buildDetectedObjects = (width, height, boxes, scores, indexes, classes) => {
+  const buildDetectedObjects = (
+    width,
+    height,
+    boxes,
+    scores,
+    indexes,
+    classes
+  ) => {
     const count = indexes.length
     const objects = []
     for (let i = 0; i < count; i++) {
@@ -171,31 +59,163 @@ class App extends React.Component {
     return objects
   }
 
+  const detect = input => {
+    const batched = tf.tidy(() => {
+      const img = tf.fromPixels(input)
+      // Reshape to a single-element batch so we can pass it to executeAsync.
+      return img.expandDims(0)
+    })
+
+    const height = batched.shape[1]
+    const width = batched.shape[2]
+
+    return model.executeAsync(batched).then(result => {
+      const scores = result[0].dataSync()
+      const boxes = result[1].dataSync()
+
+      // clean the webgl tensors
+      batched.dispose()
+      tf.dispose(result)
+
+      const [maxScores, classes] = calculateMaxScores(
+        scores,
+        result[0].shape[1],
+        result[0].shape[2]
+      )
+
+      const prevBackend = tf.getBackend()
+      // run post process in cpu
+      tf.setBackend('cpu')
+      const indexTensor = tf.tidy(() => {
+        const boxes2 = tf.tensor2d(boxes, [
+          result[1].shape[1],
+          result[1].shape[3]
+        ])
+        return tf.image.nonMaxSuppression(
+          boxes2,
+          maxScores,
+          20, // maxNumBoxes
+          0.5, // iou_threshold
+          0.5 // score_threshold
+        )
+      })
+      const indexes = indexTensor.dataSync()
+      indexTensor.dispose()
+      // restore previous backend
+      tf.setBackend(prevBackend)
+
+      return buildDetectedObjects(
+        width,
+        height,
+        boxes,
+        maxScores,
+        indexes,
+        classes
+      )
+    })
+  }
+  return {
+    detect: detect
+  }
+}
+
+class App extends React.Component {
+  videoRef = React.createRef()
+  canvasRef = React.createRef()
+
+  componentDidMount() {
+    if (navigator.mediaDevices && navigator.mediaDevices.getUserMedia) {
+      const webCamPromise = navigator.mediaDevices
+        .getUserMedia({
+          audio: false,
+          video: {
+            facingMode: 'user'
+          }
+        })
+        .then(stream => {
+          window.stream = stream
+          this.videoRef.current.srcObject = stream
+          return new Promise((resolve, _) => {
+            this.videoRef.current.onloadedmetadata = () => {
+              resolve()
+            }
+          })
+        })
+
+      const modelPromise = tf.loadFrozenModel(MODEL_URL, WEIGHTS_URL)
+      Promise.all([modelPromise, webCamPromise])
+        .then(values => {
+          this.detectFrame(this.videoRef.current, values[0])
+        })
+        .catch(error => {
+          console.error(error)
+        })
+    }
+  }
+
+  detectFrame = (video, model) => {
+    TFWrapper(model)
+      .detect(video)
+      .then(predictions => {
+        this.renderPredictions(predictions)
+        requestAnimationFrame(() => {
+          this.detectFrame(video, model)
+        })
+      })
+  }
+
+  renderPredictions = predictions => {
+    const ctx = this.canvasRef.current.getContext('2d')
+    ctx.clearRect(0, 0, ctx.canvas.width, ctx.canvas.height)
+    // Font options.
+    const font = '16px sans-serif'
+    ctx.font = font
+    ctx.textBaseline = 'top'
+    predictions.forEach(prediction => {
+      const x = prediction.bbox[0]
+      const y = prediction.bbox[1]
+      const width = prediction.bbox[2]
+      const height = prediction.bbox[3]
+      const label = LABELS[parseInt(prediction.class)]
+      // Draw the bounding box.
+      ctx.strokeStyle = '#00FFFF'
+      ctx.lineWidth = 4
+      ctx.strokeRect(x, y, width, height)
+      // Draw the label background.
+      ctx.fillStyle = '#00FFFF'
+      const textWidth = ctx.measureText(label).width
+      const textHeight = parseInt(font, 10) // base 10
+      ctx.fillRect(x, y, textWidth + 4, textHeight + 4)
+    })
+
+    predictions.forEach(prediction => {
+      const x = prediction.bbox[0]
+      const y = prediction.bbox[1]
+      const label = LABELS[parseInt(prediction.class)]
+      // Draw the text last to ensure it's on top.
+      ctx.fillStyle = '#000000'
+      ctx.fillText(label, x, y)
+    })
+  }
+
   render() {
     return (
-      <div className="Dropzone-page">
-        {this.state.model ? (
-          <MagicDropzone
-            className="Dropzone"
-            accept="image/jpeg, image/png, .jpg, .jpeg, .png"
-            multiple={false}
-            onDrop={this.onDrop}
-          >
-            {this.state.preview ? (
-              <img
-                alt="upload preview"
-                onLoad={this.onImageChange}
-                className="Dropzone-img"
-                src={this.state.preview}
-              />
-            ) : (
-              'Choose or drop a file.'
-            )}
-            <canvas id="canvas" />
-          </MagicDropzone>
-        ) : (
-          <div className="Dropzone">Loading model...</div>
-        )}
+      <div>
+        <video
+          className="size"
+          autoPlay
+          playsInline
+          muted
+          ref={this.videoRef}
+          width="600"
+          height="500"
+        />
+        <canvas
+          className="size"
+          ref={this.canvasRef}
+          width="600"
+          height="500"
+        />
       </div>
     )
   }
